86bedf8750f0f017e61057adb0010ebb17018ea4
Murtaza Amravatiwala <murtazaamravatiwala@gmail.com>
2017-04-13 07:02:22 +0000

    first django tutorial to commit

---
+++ music/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'music'
+urlpatterns = [
+    # /music/
+    url(r'^$', views.IndexView.as_view(), name="index"),
+    
+    # /music/97/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
+    
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name="album-add"),
+    
+    # /music/album/<album_id>/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name="album-update"),
+    
+    # /music/album/<album_id>/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name="album-delete"),
+    
+    #register user
+    url(r'^register/$', views.UserFormView.as_view(), name="register"),
+]
